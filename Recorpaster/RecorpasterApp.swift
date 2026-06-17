e460bfbd727db34a1828e9fa8e2978cf159dab47
Sammier <2488510850@qq.com>
2026-06-18 00:14:48 +0800

    Initial Commit

---
+++ Recorpaster/RecorpasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecorpasterApp.swift
+//  Recorpaster
+//
+//  Created by sam on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct RecorpasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
